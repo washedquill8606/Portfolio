3d5d15f71e82b3c910d4752bcba7c23407903387
Alex Puga <scdo117ds@outlook.com>
2023-02-08 15:46:29 -0600

    navbar process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import Navbar from "./scenes/Navbar"
+import { useState } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("min-width: 1060px");
   return (
-    <div className="App">
-      
+    <div className="app bg-deep-blue">
+      <Navbar>
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      </Navbar>
     </div>
   );
 }
