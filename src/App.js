9a4a1fcdf87a84b6244c316baff3e01fee2a76c6
Alex Puga <scdo117ds@outlook.com>
2023-02-08 20:55:55 -0600

    navbar integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import Navbar from "./scenes/Navbar"
-import { useState } from 'react';
-import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from "./scenes/Navbar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
+  const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("min-width: 1060px");
   return (
     <div className="app bg-deep-blue">
