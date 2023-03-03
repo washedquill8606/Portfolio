0a44ca981fd45e57313ad4deb2af29e8626adaaa
Alex Puga <scdo117ds@outlook.com>
2023-03-03 14:02:05 -0600

    skill added

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -91,6 +91,31 @@ const MySkills = () => {
           }}
         >
 
+        <img alt="django_logo"
+          className="mr-2 inline w-[24px]"
+          src="../assets/django_logo.svg"/>
+          <p className="inline font-playfair">Django</p>
+
+        <div className="mt-5">
+        <LinearProgress className="w-7/8 mb-2" variant="determinate" value={80}/>
+        <span>80%</span>
+        </div>
+        </motion.div>
+      </div>
+
+      <div className="mt-5">
+      <motion.div
+          className="md:w-7/8 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5, /*delay:1*/ }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+
         <img alt="fastapi_logo"
           className="mr-2 inline w-[24px]"
           src="../assets/fastapi_logo.svg"/>
